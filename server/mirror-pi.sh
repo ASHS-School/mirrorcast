1814151d50ffb0f62c15936c2698a503ab2c615d
Jacob Warren <3djake@gmail.com>
2017-06-05 17:35:31 +1200

    made a few tweaks

--- server/mirror-pi.sh
+++ server/mirror-pi.sh
@@ -4,10 +4,10 @@ function listen {
 	fuser 8090/tcp
 	killall omxplayer
 	sleep 2
-	omxplayer -o hdmi --lavfdopts probesize:10000 --timeout 0 -live tcp://0.0.0.0:8090?listen &
+	omxplayer -o hdmi --lavfdopts probesize:5000 --timeout 0 -live tcp://0.0.0.0:8090?listen &
 	while :
 	do
-		connection=$(netstat -tnpa 2>/dev/null| grep "0.0.0.0:8090 .* LISTEN")
+		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .* LISTEN")
 		sleep 2
 		if [[ "$connection" = "" ]]
 		then
@@ -22,13 +22,13 @@ function active {
 	sleep 5
 	while :
 	do
-		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .* ESTABLISHED")
+		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .*ESTABLISHED")
 		sleep 2
 		if [[ "$connection" = "" ]]
 		then
 			echo "killing"
 			killall omxplayer
-			fuser 8090/tcp
+			fuser -k 8090/tcp
 			break
 		fi
 	done
