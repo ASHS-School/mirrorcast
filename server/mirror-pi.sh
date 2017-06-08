bf9527a76153e0446aa8e507a6bdd52e99af50b6
Jacob Warren <3djake@gmail.com>
2017-06-08 17:30:20 +1200

    automate turning display on

--- server/mirror-pi.sh
+++ server/mirror-pi.sh
@@ -4,6 +4,8 @@ function listen {
 	fuser -k 8090/tcp
 	killall omxplayer*
 	sleep 2
+	#You can change the position and dimesions of the output with --win "x1 y1 x2 y2"
+	#For example, you might want this for projectors that overshoot their screens
 	nohup omxplayer -o hdmi --lavfdopts probesize:5000 --timeout 0 -live tcp://0.0.0.0:8090?listen &
 	while :
 	do
@@ -12,7 +14,10 @@ function listen {
 		sleep 1
 		if [[ "$connection" = "" ]]
 		then
+			echo "on 0" | cec-client -s
 			echo "established"
+			sleep 1
+			echo "as" | cec-client -s
 			break
 		fi
 	done
@@ -28,6 +33,7 @@ function active {
 		sleep 1
 		if [[ "$connection" = "" ]]
 		then
+			echo "standby 0" | cec-client -s
 			echo "connection dropped"
 			killall omxplayer*
 			fuser -k 8090/tcp
