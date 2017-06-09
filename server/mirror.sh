cd6d647a7fecbc85f5bf489edf235a458528d097
Jacob Warren <3djake@gmail.com>
2017-06-09 16:12:40 +1200

    tweaks

--- server/mirror.sh
+++ server/mirror.sh
@@ -3,13 +3,15 @@
 function listen {
 	killall ffplay*
 	fuser -k 8090/tcp
-	nohup ffplay -fs -probesize 8000 -sync ext tcp://0.0.0.0:8090?listen &
+	sleep 1
+	nohup ffplay -fs -probesize 7000 -sync ext tcp://0.0.0.0:8090?listen &
+	sleep 1
 	while :
 	do
 		sleep 1
 		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .* LISTEN")
 		sleep 1
-		if [[ "$connection" = "" ]]
+		if [[ $connection = "" ]]
 		then
 			echo "established"
 			break
@@ -25,7 +27,7 @@ function active {
 		sleep 1
 		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .*ESTABLISHED")
 		sleep 1
-		if [[ "$connection" = "" ]]
+		if [[ $connection = "" ]]
 		then
 			echo "lost connection"
 			killall ffplay*
