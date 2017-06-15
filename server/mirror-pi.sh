956899c0558b8479571e5a66f79ddf1ba5220991
Jacob Warren <3djake@gmail.com>
2017-06-16 04:17:03 +1200

    kill connection if client suspends without clicking stop

--- server/mirror-pi.sh
+++ server/mirror-pi.sh
@@ -3,9 +3,10 @@
 function listen {
 	fuser -k 8090/tcp
 	killall omxplayer*
-	sleep 2
+	sleep 1
 	#You can change the position and dimesions of the output with --win "x1 y1 x2 y2"
 	#For example, you might want this for projectors that overshoot their screens
+	sleep 1
 	nohup omxplayer -o hdmi --lavfdopts probesize:5000 --timeout 0 -live tcp://0.0.0.0:8090?listen &
 	while :
 	do
@@ -25,13 +26,14 @@ function listen {
 }
 
 function active {
-	sleep 5
 	while :
 	do
 		sleep 1
 		connection=$(netstat -tnpa 2>/dev/null| grep ":8090 .*ESTABLISHED")
+		client=$(netstat -tnpa 2>/dev/null|grep -o -P '(?<=8090).*(?=:)')
+		ping=$(ping -c 1 -W 1 $client |grep "1 received")
 		sleep 1
-		if [[ "$connection" = "" ]]
+		if [[ "$connection" = "" || $ping = "" ]]
 		then
 			echo "standby 0" | cec-client -s
 			echo "connection dropped"
