8499a905e470d4380160e7db2d47b5e2aa1318d8
Jacob Warren <3djake@gmail.com>
2017-08-24 04:07:42 +1200

    logging update

--- client/hosts.py
+++ client/hosts.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-import logging, os, csv
+import logging, os, csv, logging.handlers
 
-logging.basicConfig(filename='/opt/mirrorcast/mirrorcast.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
+mirror_logger = logging.getLogger()
+mirror_logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+formatter = logging.Formatter(' mirrorcast - %(name)s -  %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+mirror_logger.addHandler(handler)
+
+#logging.basicConfig(filename='/opt/mirrorcast/mirrorcast.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 
 class Hosts():
     def __init__(self):
@@ -15,10 +22,9 @@ class Hosts():
                 for line in file:
                     self.receivers.append(line)
         except:
-            logging.error("Failed to load host names")
+            mirror_logger.error("Failed to load host names")
             exit(0)
         csvfile.close()
-        print("init")
         self.aspect = self.receivers[0]['aspect']
     
     #set receiver to the one picked by the user     
@@ -27,9 +33,9 @@ class Hosts():
         for i in self.receivers:
             if i['host'] == self.receiver and but.get_active():
                 self.aspect = i['aspect']
-                logging.info("Receiver set to: " + i['host'] + " Receivers aspect: " + self.aspect)
+                mirror_logger.info("Receiver set to: " + i['host'] + " Receivers aspect: " + self.aspect)
                 return
         if but.get_active():
             self.receiver = "None"
             self.aspect = "16:9"
-            logging.info("Receiver set to: " + self.receiver)
+            mirror_logger.info("Receiver set to: " + self.receiver)
