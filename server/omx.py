e49453c5f0a048fd0574fda3c27d0cf99c94b54b
Jacob Warren <3djake@gmail.com>
2017-08-27 08:57:43 +1200

    manage all omxplayer processes via wrapper

---
+++ server/omx.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from omxplayer import OMXPlayer
+import subprocess
+
+class Omx():
+    def __init__(self):
+        self.url = "None"
+        self.player = None
+       
+    def youtube(self):
+        proc = subprocess.Popen(['youtube-dl', '-g', '-f', 'mp4', self.url], stdout=subprocess.PIPE)
+        url = proc.stdout.read()
+        self.player = OMXPlayer(url.decode("utf-8", "strict")[:-1])
+
+    def start_media(self, host):
+        address = "http://" + str(host) + ":8090/video"
+        self.player = OMXPlayer(address, args=['-o', 'hdmi', '--timeout',  '30', '--lavfdopts', 'probesize:30000', '--threshold', '2'])            
+    
+    def mirror(self):
+        self.player = OMXPlayer("udp://0.0.0.0:8090?listen", args=['-o', 'hdmi', '--lavfdopts', 'probesize:8000', '--timeout', '0', '--threshold', '0'])
+    
