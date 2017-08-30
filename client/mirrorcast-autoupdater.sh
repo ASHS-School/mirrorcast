cb87fa97d07c2e59d9580150f392175ebf322cb3
Jacob Warren <3djake@gmail.com>
2017-08-30 12:27:11 +1200

    Simple way for user to update client

--- client/mirrorcast-autoupdater.sh
+++ client/mirrorcast-autoupdater.sh
@@ -2,6 +2,6 @@
 
 wget http://epick.org/mirrorcast/mirrorcast-latest.deb -O /tmp/mirrorcast-latest.deb
 
-gksudo dpkg -i -E /tmp/mirrorcast-latest.deb
+cp /opt/mirrorcast/receivers /tmp/
 
-python3 /opt/mirrorcast/mirrorcast-client.py &
+gksudo -- bash -c 'dpkg -i -E /tmp/mirrorcast-latest.deb; cp /tmp/receivers /opt/mirrorcast/'
