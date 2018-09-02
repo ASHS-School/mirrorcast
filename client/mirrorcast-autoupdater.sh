0e16e88f53e291f75f71b77531f7574ecc843292
Jacob Warren <3djake@gmail.com>
2018-09-02 18:06:21 +1200

    Moved and sorted files

--- client/mirrorcast-autoupdater.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget http://epick.org/mirrorcast/mirrorcast-latest.deb -O /tmp/mirrorcast-latest.deb
-
-cp /opt/mirrorcast/receivers /tmp/
-
-gksudo -- bash -c 'dpkg -i -E /tmp/mirrorcast-latest.deb; cp /tmp/receivers /opt/mirrorcast/'
