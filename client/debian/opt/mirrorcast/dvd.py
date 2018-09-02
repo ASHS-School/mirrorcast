80c8539efd725da75601a312cd4bb18b67d06e27
Jacob Warren <3djake@gmail.com>
2018-09-02 18:11:12 +1200

    Added DVD playback

--- client/debian/opt/mirrorcast/dvd.py
+++ client/debian/opt/mirrorcast/dvd.py
@@ -14,37 +14,38 @@ class Dvd(object):
         self.l.pack()
         self.l3=Label(topframe,text="__________________________________________")
         self.l3.pack()
-        self.l2=Label(topframe,text="Note: It may take up to 15 seconds to start your media on the receiver")
+        self.l2=Label(topframe,text="Note: It may take up to 15 seconds to start your DVD on the receiver")
         self.l2.pack()
-        self.l4=Label(topframe,text="This feature is experimental, if you file does not play please close this window and try again.")
+        self.l4=Label(topframe,text="This feature is experimental, if your DVD does not play then you might be missing some dependencies.")
         self.l4.pack()
         controls = Frame(top)
         controls.pack(side=BOTTOM)
-
+        controls2 = Frame(top)
+        controls2.pack(side=BOTTOM)
         self.state=Label(controls,textvariable="")
         self.backb=Button(controls,text='Rewind',command=self.back)
         self.playb=Button(controls,text='Play/Pause',command=self.play)
         self.forwardb=Button(controls,text='Fast-Forward',command=self.forward)
         self.stopb=Button(controls,text='Stop',command=self.stop)
-        self.volb = Scale(controls, from_=-2500, to=400, orient=HORIZONTAL, label="Volume", showvalue=0, command=self.vol)
+        #self.volb = Scale(controls, from_=0, to=110, orient=HORIZONTAL, label="Volume", showvalue=0, command=self.vol)
         self.state.pack(side=LEFT)
         self.backb.pack(side=LEFT)
         self.playb.pack(side=LEFT)
         self.forwardb.pack(side=LEFT)
         
         
-        self.audiobackb=Button(controls,text='Audio Track Back',command=self.audioback)
-        self.audioforwb=Button(controls,text='Audio Track Up',command=self.audioforw)
-        self.pchaptb=Button(controls,text='Previous Chapter',command=self.p_chapt)
-        self.nchaptb=Button(controls,text='Next Chapter',command=self.n_chapt)
-        self.subtitleb=Button(controls,text='Subtitle',command=self.subtitle)
-        self.audioforwb.pack(side=LEFT)
+        self.audiobackb=Button(controls2,text='Audio Track Back',command=self.audioback)
+        self.audioforwb=Button(controls2,text='Audio Track Up',command=self.audioforw)
+        self.pchaptb=Button(controls2,text='Previous Chapter',command=self.p_chapt)
+        self.nchaptb=Button(controls2,text='Next Chapter',command=self.n_chapt)
+        self.subtitleb=Button(controls2,text='Subtitle',command=self.subtitle)
         self.audiobackb.pack(side=LEFT)
+        self.audioforwb.pack(side=LEFT)
         self.pchaptb.pack(side=LEFT)
         self.nchaptb.pack(side=LEFT)
         self.subtitleb.pack(side=LEFT)
-        self.volb = Scale(controls, from_=-2500, to=400, orient=HORIZONTAL, label="Volume", showvalue=0, command=self.vol)
-        self.volb.set(0)
+        self.volb = Scale(controls, from_=0, to=110, orient=HORIZONTAL, label="Volume", showvalue=0, command=self.vol)
+        self.volb.set(100)
         self.volb.pack()
     def vol(self, vol):
         cmd = "dvd-vol,"
@@ -71,15 +72,15 @@ class Dvd(object):
         self.send_cmd(cmd)
         return
     def n_chapt(self):
-        cmd = "n_chapt,"
+        cmd = "dvd-n-chapt,"
         self.send_cmd(cmd)
         return
     def p_chapt(self):
-        cmd = "p_chapt,"
+        cmd = "dvd-p-chapt,"
         self.send_cmd(cmd)
         return
     def subtitle(self):
-        cmd = "subtitle,"
+        cmd = "dvd-subtitle,"
         self.send_cmd(cmd)
         return
     def stop(self):
